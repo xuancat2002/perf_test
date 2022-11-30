e8b2be4f6e3195929667ab1b387258c26ceafe05
Weixing Sun <weixingsun@users.noreply.github.com>
2022-11-30 15:58:52 +0900

    Update loop_docker.sh

--- va1v_BD/script/loop_docker.sh
+++ va1v_BD/script/loop_docker.sh
@@ -6,12 +6,13 @@ for i in $(seq 0 $INDEX); do
   echo "video card$i"
   #./start_perf.sh $i        # cpu.csv/pcie.csv/mem.csv
   i_1=$((i-1))
-  for j in $(seq 0 $i_1); do
+  if [ $i -lt $INDEX ]; then
     echo "./docker_video.sh $j $CASE > video_card$i.$j.log 2>&1 &"
          ./docker_video.sh $j $CASE > video_card$i.$j.log 2>&1 &
-  done
-  echo "./docker_video.sh $i $CASE > video_card$i.$i.log last"
-       ./docker_video.sh $i $CASE > video_card$i.$i.log 2>&1
+  else
+    echo "./docker_video.sh $i $CASE > video_card$i.$i.log last"
+         ./docker_video.sh $i $CASE > video_card$i.$i.log 2>&1
+  fi
   #./stop_perf.sh $i
   sleep 88
 done
