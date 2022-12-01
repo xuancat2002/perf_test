413ba35dd7e40c8401ad14684cfb47db0a03fb26
Weixing Sun <weixingsun@users.noreply.github.com>
2022-12-01 12:51:26 +0900

    Update docker_video.sh

--- script/docker_video.sh
+++ script/docker_video.sh
@@ -50,7 +50,8 @@ else
     ["720"]="vidyo1_1280x720_30fps_5M_60400frames.mp4"
   	["1080"]="cdzi-hevc.mp4"  #1080p
     ["1k"]="ParkScene_1920x1080_30fps_loop_8Mx4.mp4"
-  	["4k"]="4KH264_5.0Mbps_30fps_8bit_Brazil_5M_3840x2160.mp4"
+    ["2k"]="4KH264_5.0Mbps_30fps_8bit_Brazil_5M_3840x2160.mp4"
+    ["4k"]="Winter_Saint_Petersburg_Russia_h264_7680x4320_loop5.mp4"
   	["8k"]="Winter_Saint_Petersburg_Russia_1500frames_h264_7680x4320.mp4"
   )
   DIR=/opt/vastai/vaststream/samples/
