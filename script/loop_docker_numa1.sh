6bda1092935120d2ae154a3dfe50364464dc5f34
Weixing Sun <weixingsun@users.noreply.github.com>
2022-12-10 13:40:54 +0900

    Update loop_docker_numa1.sh

--- script/loop_docker_numa1.sh
+++ script/loop_docker_numa1.sh
@@ -15,3 +15,8 @@ fi
    i=3
    echo "./docker_video.sh $i $PARAM > video_card$i.$CASE.log last"
          ./docker_video.sh $i $PARAM > video_card$i.$CASE.log 2>&1
+
+sleep 30
+cat  video_card2.$CASE.log | sed -e 's/\r/\n/g'|grep ^frame= |awk -F= '{print $3}'| awk '{ total += $1; count++ } END { print total/count }' > frame2.log
+cat  video_card3.$CASE.log | sed -e 's/\r/\n/g'|grep ^frame= |awk -F= '{print $3}'| awk '{ total += $1; count++ } END { print total/count }' > frame3.log
+no_tmpfs: fps=32.0148
