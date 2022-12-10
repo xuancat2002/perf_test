842b84d05dda9593dcd921446787113eb45fd73d
Weixing Sun <weixingsun@users.noreply.github.com>
2022-12-10 13:49:41 +0900

    sar-d

--- script/start_perf.sh
+++ script/start_perf.sh
@@ -7,10 +7,12 @@ CARD=${1:-va1v_ai0}
 DIR="logs/$CARD"
 
 mkdir -p $DIR
+PCIE=`lspci|grep acc|awk '{print $1}'| tr '\n' ','`
 #vasmi dmon -d 0,1,2,3  > $DIR/dmon.log 2>&1 &
-mpstat -P ALL 2     > $DIR/cpu.csv 2>&1 &
-pmt --delay=2 --output=$DIR/mem.csv > /dev/null 2>&1 &
-pcie --only=`lspci|grep acc|awk '{print $1}'| tr '\n' ','` --output=$DIR/pcie.csv > /dev/null 2>&1 &
+mpstat -P ALL 2      > $DIR/cpu.csv  2>/dev/null &
+sar -d 2             > $DIR/disk.csv 2>/dev/null &
+pmt  --delay=2    --output=$DIR/mem.csv  > /dev/null 2>&1 &
+pcie --only=$PCIE --output=$DIR/pcie.csv > /dev/null 2>&1 &
 #pcm 2 -nc -nsys  -csv=$DIR/cpu.csv  > /dev/null 2>&1 &
 #pcm-pcie     -B  -csv=$DIR/pcie.csv > /dev/null 2>&1 &
 #pcm-memory 2 -nc -csv=$DIR/mem.csv  > /dev/null  2>&1 &
