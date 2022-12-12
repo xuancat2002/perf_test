c54fecc41414f22a06ff8a2203ea9e3f3d3454f7
Weixing.Sun <weixing.sun@gmail.com>
2022-12-12 14:46:52 +0800

    loop fix

--- script/loop_docker_resnet50.sh
+++ script/loop_docker_resnet50.sh
@@ -4,8 +4,8 @@ date
 COUNT=`vasmi summary|grep VA1|wc -l`
 INDEX=$((COUNT-1))
 
-#NAME=`echo $CASE | awk -F. '{print $1}'`
-#OPT=`echo $CASE | awk -F. '{print $2}'`
+NAME=`echo $CASE | awk -F. '{print $1}'`
+OPT=`echo $CASE | awk -F. '{print $2}'`
 #PARAM=`echo $OPT | sed 's/_/ /g'`
 
 FC=`ls /data|wc -l`
@@ -21,11 +21,11 @@ fi
 for i in $(seq 0 $INDEX); do
   echo "ai card$i"
   if [ $i -lt $INDEX ]; then
-    echo "./docker_resnet50.sh $i $CASE > resnet_card$i.$CASE.log 2>&1 &"
-          ./docker_resnet50.sh $i $CASE > resnet_card$i.$CASE.log 2>&1 &
+    echo "./docker_resnet50.sh $i $NAME $OPT > resnet_card$i.$CASE.log 2>&1 &"
+          ./docker_resnet50.sh $i $NAME $OPT > resnet_card$i.$CASE.log 2>&1 &
   else
-    echo "./docker_resnet50.sh $i $CASE > resnet_card$i.$CASE.log last"
-          ./docker_resnet50.sh $i $CASE > resnet_card$i.$CASE.log 2>&1
+    echo "./docker_resnet50.sh $i $NAME $OPT > resnet_card$i.$CASE.log last"
+          ./docker_resnet50.sh $i $NAME $OPT > resnet_card$i.$CASE.log 2>&1
   fi
   sleep 1
 done
