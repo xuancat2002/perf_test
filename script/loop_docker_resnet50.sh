99712ad43e4ebfb89ecb426e744b1902ebfe6f72
weixingsun <weixing.sun@gmail.com>
2022-12-12 14:38:53 +0900

    add resnet50 loop

---
+++ script/loop_docker_resnet50.sh
@@ -0,0 +1,35 @@
+CASE=${1:-1k}      # test case
+date
+COUNT=`vasmi summary|grep VA1|wc -l`
+INDEX=$((COUNT-1))
+
+#NAME=`echo $CASE | awk -F. '{print $1}'`
+#OPT=`echo $CASE | awk -F. '{print $2}'`
+#PARAM=`echo $OPT | sed 's/_/ /g'`
+
+FC=`ls /data|wc -l`
+if [ $FC -lt 1 ]; then
+  mount -t nfs 192.168.20.2:/nfs/sedata /data
+fi
+DC=`docker ps|wc -l`
+if [ $DC -gt 1 ]; then
+  docker stop `docker ps -aq`
+fi
+
+for i in $(seq 0 $INDEX); do
+  echo "ai card$i"
+  if [ $i -lt $INDEX ]; then
+    echo "./docker_resnet50.sh $i $CASE > resnet_card$i.$CASE.log 2>&1 &"
+          ./docker_resnet50.sh $i $CASE > resnet_card$i.$CASE.log 2>&1 &
+  else
+    echo "./docker_resnet50.sh $i $CASE > resnet_card$i.$CASE.log last"
+          ./docker_resnet50.sh $i $CASE > resnet_card$i.$CASE.log 2>&1
+  fi
+  sleep 1
+done
+
+#sleep 30
+#for f in `ls resnet_card*log`; do 
+#  fps=`cat $f | sed -e 's/\r/\n/g'|grep ^frame= |awk -F= '{print $3}'| awk '{ total += $1; cnt++ } END { print total/cnt }'`
+#  echo "$f fps=$fps" >> logs/$CASE/fps.log
+#done
