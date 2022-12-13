7b99bb1345733fafb5af028f788b8a7e230f22ce
Weixing Sun <weixingsun@users.noreply.github.com>
2022-12-14 00:39:11 +0900

    Update docker_ai_cnn.sh

--- script/docker_ai_cnn.sh
+++ script/docker_ai_cnn.sh
@@ -1,12 +1,60 @@
-#docker run --rm -itd --name bm0 --net=host --runtime=vastai -e VASTAI_VISIBLE_DEVICES=0 -v /etc/localtime:/etc/localtime -v /opt/data:/opt/data reg.devops.com/base/benchmark:v1.2.2
 
-#docker exec bm0 bash -c "source /etc/profile; cd /opt/benchmark; python3 main.py -m /opt/data/v1.2.2/mobilenet_v1"
-#docker exec bm0 bash -c "source /etc/profile; cd /opt/benchmark; python3 main.py -m /opt/data/v1.2.2/mobilenet_v2"
+IDX=${1:-0}
+MODE=${2:-mobilenet_v1}
 
 ITER=1000000
 PERF=/opt/benchmark/conf/mobilenet/perf
-MODEL=mobilenet_v1-keras-keras-fp16-none-224_224-runstream-1.json
-#MODEL=mobilenet_v2-timm-onnx-int8-percentile-224_224-runstream-1.json
 
-docker exec bm0 bash -c "source /etc/profile; /opt/vaststream/tool/vaTest -d 0 -b 100 -v 1 -p -1 -i $ITER -j $PERF/$MODEL --batch 1"
+AI_ImageID= reg.devops.com/base/benchmark:v1.2.2
+DR_NAME=`rpm -qa|grep vastai-pci`
+if [ "$DR_NAME" = "vastai-pci-sw-v1-1-alpha-hwtype-2-00.22.09.05-1dkms.x86_64" ]; then
+  AI_ImageID="744a372861d7"    # perf test image
+  docker_image="/data/driver/bert_2209/bert_test.tar"
+elif [ "$DR_NAME" = "vastai-pci-sw-v1-1-alpha-hwtype-2-00.22.09.05-1dkms.x86_64" ]; then
+  AI_ImageID="9cd2e9543f99"    # accuracy test image
+  docker_image="192.168.20.143:443/vaststream/vaststream_ubuntu18.04:V_202208220913_f579cfa5_sw_v1_1_alpha"
+fi
 
+list_image_id=`docker images | grep -i $AI_ImageID | awk '{print$3}'`
+if [ "$list_image_id" = "$AI_ImageID" ]; then
+	echo "-------------Already load--------------------"
+else
+	echo "-------------Load Docker---------------------"
+	#docker pull $docker_image
+	docker load -i $docker_image
+fi
+host_dataset_path=/opt/data
+#DATA_SRC=/data/ai_dataset/2012img.tgz
+EXEC=/opt/vastai/vaststream/release/samples/05_Bert2Die
+NIDX=$((IDX+1))
+PCI_N=`lspci|grep accelerators | sed -n ${NIDX}p|awk '{print $1}'`
+NODE=`cat /sys/bus/pci/devices/0000:$PCI_N/numa_node`
+cgcreate -g cpuset:numanode$NODE
+CPUS1=`lscpu|grep node$NODE|awk '{print $4}'`
+echo "$CPUS1" > /sys/fs/cgroup/cpuset/numanode$NODE/cpuset.cpus
+echo "$NODE" > /sys/fs/cgroup/cpuset/numanode$NODE/cpuset.mems
+
+
+docker run --rm -itd --name ai_card${IDX} \
+  --cgroup-parent=numanode${NODE} --net=host \
+  --runtime=vastai -e VASTAI_VISIBLE_DEVICES=${IDX} \
+   -v /etc/localtime:/etc/localtime -v /opt/data:/opt/data \
+  ${AI_ImageID} /bin/bash
+sleep 5
+
+#docker run --rm -itd --name bm0 --net=host --runtime=vastai -e VASTAI_VISIBLE_DEVICES=0 -v /etc/localtime:/etc/localtime -v /opt/data:/opt/data reg.devops.com/base/benchmark:v1.2.2
+
+if [ "$MODE" = "mobilenet_v1" ]; then
+  MODEL=mobilenet_v1-keras-keras-fp16-none-224_224-runstream-1.json
+  docker exec ai_card$IDX bash -c "source /etc/profile; cd /opt/benchmark; python3 main.py -m /opt/data/v1.2.2/mobilenet_v1"
+
+elif [ "$MODE" = "mobilenet_v2" ]; then
+  MODEL=mobilenet_v2-timm-onnx-int8-percentile-224_224-runstream-1.json
+  docker exec ai_card$IDX bash -c "source /etc/profile; cd /opt/benchmark; python3 main.py -m /opt/data/v1.2.2/mobilenet_v2"
+
+fi
+
+docker exec ai_card$IDX bash -c "source /etc/profile; /opt/vaststream/tool/vaTest -d 0 -b 100 -v 1 -p -1 -i $ITER -j $PERF/$MODEL --batch 1"
+
+#docker cp $host_dataset_path/load_bert.sh ai_card${IDX}:$EXEC
+#docker exec ai_card$IDX bash -c "source /etc/profile; cd $EXEC; ./load_bert.sh 30"
