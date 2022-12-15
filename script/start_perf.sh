42ff610dcebdbd2af5b6adc2e4b704486c4b0ed9
Weixing Sun <weixingsun@users.noreply.github.com>
2022-12-15 23:48:00 +0900

    Update start_perf.sh

--- script/start_perf.sh
+++ script/start_perf.sh
@@ -23,11 +23,11 @@ PCIE=`lspci|grep acc|awk '{print $1}'| tr '\n' ','`
 CNT=`lspci|grep acc|wc -l`
 CNT1=$((CNT-1))
 DEVS=`echo $(seq 0 $CNT1)|tr ' ' ','`
-vasmi dmon -d $DEVS -i 0,1 > $DIR/dmon.log  2>&1 &
-mpstat -P ALL 2      > $DIR/cpu.csv  2>/dev/null &
-sar -d 2             > $DIR/disk.csv 2>/dev/null &
-pmt  --delay=2    --output=$DIR/mem.csv  > /dev/null 2>&1 &
-pcie --only=$PCIE --output=$DIR/pcie.csv > /dev/null 2>&1 &
+mpstat -P ALL 2                  > $DIR/cpu.csv  2>/dev/null &
+sar -d 2                         > $DIR/disk.csv 2>/dev/null &
+$BIN/vasmi dmon -d $DEVS -i 0,1  > $DIR/dmon.log 2>/dev/null &
+$BIN/mem  --delay=2    --output=$DIR/mem.csv  > /dev/null 2>&1 &
+$BIN/pcie --only=$PCIE --output=$DIR/pcie.csv > /dev/null 2>&1 &
 #pcm 2 -nc -nsys  -csv=$DIR/cpu.csv  > /dev/null 2>&1 &
 #pcm-pcie     -B  -csv=$DIR/pcie.csv > /dev/null 2>&1 &
 #pcm-memory 2 -nc -csv=$DIR/mem.csv  > /dev/null  2>&1 &
