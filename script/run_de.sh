52d6d1afb37bb257800a5bd0c53277b9ec3b08a9
Weixing Sun <weixingsun@users.noreply.github.com>
2022-12-26 11:42:34 +0900

    Update run_de.sh

--- script/run_de.sh
+++ script/run_de.sh
@@ -89,10 +89,10 @@ starttime=`date +%s%3N`
        if [ "$issave" == 'y' ]; then
            #mkdir -p performance/$j
            #./run_de_perf.sh $file 1 ./performance/$j/$json_name"_performance.txt" | tee current_test_round$j/$json_name".txt"
-           ./run_de_perf.sh $file 1 /dev/null | tee current_test_round$j/$json_name".txt"
+           ./run_de_perf.sh $file 1 /dev/null 
            #check_performance ./performance/$j/$json_name"_performance.txt" $j $file
        else
-           ./run_de_perf.sh $file 1 | tee current_test_round$j/$json_name".txt"
+           ./run_de_perf.sh $file 1 
        fi
        end=`date +%s%3N`
        totaltime=`expr $end - $start`
